01a24af0edbd749daada8dfe332d17269b6bee94
Paulius Karpavicius <71287436+sielos@users.noreply.github.com>
2021-11-21 18:56:37 +0000

    added spotting

--- ca_descriptions/forest_fire_2d.py
+++ ca_descriptions/forest_fire_2d.py
@@ -6,44 +6,112 @@ import sys
 import inspect
 import random
 import scipy.stats
+
 this_file_loc = (inspect.stack()[0][1])
 main_dir_loc = this_file_loc[:this_file_loc.index('ca_descriptions')]
 sys.path.append(main_dir_loc)
 sys.path.append(main_dir_loc + 'capyle')
 sys.path.append(main_dir_loc + 'capyle/ca')
 sys.path.append(main_dir_loc + 'capyle/guicomponents')
-# ---
+# ---   
 
 from capyle.ca import Grid2D, Neighbourhood, CAConfig, randomise2d
 import capyle.utils as utils
 import numpy as np
+import math
+from matplotlib import pyplot as plt
+from scipy.ndimage.filters import gaussian_filter
+
+def transition_func(grid, neighbourstates, neighbourcounts, decaygrid, initial_terrain, topology_grid, count):
+    count[0] = count[0] + 1
+
+    terrain_fire_rates = {0: 0.05, 1: 0, 2: 0.05, 3: 1, 4: 0, 5: 0}
+
+    def lightning_strike():
+        rows, cols = 200, 200
+        upper_lake_x1, upper_lake_x2 = int(0.15*cols), int(0.2*cols)
+        upper_lake_y1, upper_lake_y2 = rows-int(0.7*rows), rows-int(0.6*rows)
+
+        lower_lake_x1, lower_lake_x2 = int(0.6*cols), int(0.9*cols)
+        lower_lake_y1, lower_lake_y2 = rows-int(0.35*rows), rows-int(0.3*rows)
+
+        x, y = (random.randint(0, 199), random.randint(0, 199))
+        # test values
+        # x, y = 35, 65
+        # x, y = 100, 100
+        
+        # check if generate coordinates are in water, if so then lightning has no effect
+        if ((upper_lake_x1 <= x <= upper_lake_x2 and upper_lake_y1 <= y <= upper_lake_y2) or 
+            (lower_lake_x1 <= x <= lower_lake_x2 and lower_lake_y1 <= y <= lower_lake_y2)):
+            grid[y][x] = 1
+        else:
+            grid[y][x] = 5
+
+    def find_random_burning_cell(array):
+        (burning_x, burning_y) = random.choice(array)
+        return burning_x, burning_y
+
+    def spotting(burning_cell, wind_direction, wind_speed):
+        x, y = burning_cell
+        distance_x, distance_y = x*wind_direction[0], y*wind_direction[1]
+
+        if distance_x < 0:
+            distance_x *= -1
+        elif distance_x > 0:
+            distance_x = 200 - distance_x
+
+        if distance_y < 0:
+            distance_y *= -1
+        elif distance_y > 0:
+            distance_y = 200 - distance_y
+
+        print(x, y, distance_x, distance_y)
 
 
-def transition_func(grid, neighbourstates, neighbourcounts, decaygrid, initial_terrain):
+        relative_dist_x = random.randint(0, int(distance_x * (wind_speed / 25)))
+        relative_dist_y = random.randint(0, int(distance_y * (wind_speed / 25)))
 
-    def decide(probability):
+        print('relative distance')
+        print(relative_dist_x, relative_dist_y)
+
+        x = x + relative_dist_x*wind_direction[0]
+        y = y + relative_dist_y*wind_direction[1]
+
+        print('spotted cell')
+        print(x, y)
+        if probability_p_burn(y, x):
+            print('pavyko!!!!!1')
+        grid[y][x] = 5
+
+
+
+
+    def decision(probability):
         return random.random() < probability
-
+    
     def probability_p_burn(x, y):
+
         terrain = grid[x][y]
 
-        wind_probabilities = probability_p_w([0, -1], 6.0, fire_direction(x, y))
+        wind_probabilities = probability_p_w([1, 0], 5.0, fire_direction(x, y))
         p_h, p_veg, p_den, p_w, p_s = (0, 0, 0, 0.0, 0)
-        decision = False
+        decide = False
+
+        p_s = calculate_slope_fire_spread_probability(x, y)
 
         for p_w in wind_probabilities:
             if terrain == 0:
-                p_h, p_veg, p_den, p_s = (0.5, 0.5, 0.5, 0.5)
+                p_h, p_veg, p_den, p_s = (0.5, 0.5, 0.5, p_s)
             elif terrain == 2:
-                p_h, p_veg, p_den, p_s = (0.05, 0.05, 0.05, 0.5)
+                p_h, p_veg, p_den, p_s = (0.05, 0.05, 0.05, p_s)
             elif terrain == 3:
                 p_h, p_veg, p_den, p_s = (1, 1, 1, 1)
 
-            decision = decide(p_h * (1 + p_veg) * (1 + p_den) * p_w * p_s)
-            if decision:
+            decide = decision(p_h * (1 + p_veg) * (1 + p_den) * p_w * p_s)
+            if decide:
                 break
 
-        return decision
+        return decide
 
     def probability_p_w(wind_direction, wind_speed, relative_fire_coordinates):
         wind_probabilities = []
@@ -55,6 +123,46 @@ def transition_func(grid, neighbourstates, neighbourcounts, decaygrid, initial_t
 
         return wind_probabilities
 
+    def calculate_slope_fire_spread_probability(row, column):
+
+        # Calculate final fire rate probability due to elevation
+        neighbour_terrain_fire_spread_probabilities = calculate_terrain_spread_probabilities(row, column)
+        #print("MAX: ", max(neighbour_terrain_fire_spread_probabilities))
+        # See if slope small enough to make block catch fire
+        on_fire_threshold = 0.1
+        cells_that_should_spread_fire = np.array(neighbour_terrain_fire_spread_probabilities) < on_fire_threshold
+
+        #print("LEN:", (cells_that_should_spread_fire == True).size / 8)
+        return (cells_that_should_spread_fire == True).size / 8
+
+    def calculate_terrain_spread_probabilities(row, column):
+        
+        def calculate_slope(current_cell, neighbour_cell, is_diagonal):
+            if is_diagonal:
+                return (current_cell, math.degrees(math.atan((current_cell - neighbour_cell) / math.sqrt(2))))
+            else:
+                return (current_cell, math.degrees(math.atan((current_cell - neighbour_cell) / 1)))
+
+        terrain = grid[row][column]
+        
+         # Non Diagonal
+        up_slope = calculate_slope(terrain, topology_grid[row][col + 1], False)
+        down_slope = calculate_slope(terrain, topology_grid[row][col - 1], False)
+        right_slope = calculate_slope(terrain, topology_grid[row + 1][col], False)
+        left_slope = calculate_slope(terrain, topology_grid[row - 1][col], False)
+
+        # Diagonal
+        top_left_slope = calculate_slope(terrain, topology_grid[row - 1][col - 1], True)
+        bottom_left_slope = calculate_slope(terrain, topology_grid[row + 1][col - 1], True)
+        top_right_slope = calculate_slope(terrain, topology_grid[row - 1][col + 1], True)
+        bottom_right_slope = calculate_slope(terrain, topology_grid[row + 1][col + 1], True)
+
+        slopes = [up_slope, down_slope, left_slope, right_slope, top_left_slope, top_right_slope, bottom_left_slope, bottom_right_slope]
+
+        fire_probabilities = list(map(lambda x: terrain_fire_rates[x[0]] * math.exp(1 * x[1]), slopes))
+
+        return fire_probabilities
+    
     def angle_between_two_vectors(v1, v2):
         def unit_vector(vector):
             return np.divide(vector, np.linalg.norm(vector))
@@ -73,8 +181,9 @@ def transition_func(grid, neighbourstates, neighbourcounts, decaygrid, initial_t
         return relative_fire_coordinates
 
     np.seterr(invalid='ignore')  # ignores warnings lol
+    #grid[y axis][x axis]
 
-    # cells that can catch fire: chaparral, forest, scrubland and have a neighbour that's on fire
+    # cells that can catch fire: chaparral, forest, scrubland and have a neighbour thats on fire
     burnable_cells = (((grid == 0) | (grid == 2) | (grid == 3)) & (neighbourcounts[5] >= 1))
 
     # cells that were burning in last time step
@@ -82,18 +191,18 @@ def transition_func(grid, neighbourstates, neighbourcounts, decaygrid, initial_t
 
     # take one off their decay value, calculates for how many time steps a cell has been burning
     decaygrid[burning_cells] -= 1
-    count = 0
     # assigns the probability of each cell catching fire and then probabilistically decides if it will
+    burning_array = []
     for row in range(0, 199):
         for col in range(0, 199):
+            if grid[col][row] == 5:
+                burning_array.append((row, col))
             if row == 0 or col == 0 or burnable_cells[col][row] is False:
                 burnable_cells[col][row] = False
             elif burnable_cells[col][row]:
-                count = count + 1
                 burnable_cells[col][row] = probability_p_burn(col, row)
 
-    print(count)
-
+    print(count - 1)
     # prevents fire from spreading on the other side of the map
     burnable_cells[0] = False
     burnable_cells[199] = False
@@ -109,6 +218,11 @@ def transition_func(grid, neighbourstates, neighbourcounts, decaygrid, initial_t
 
     grid[decayed_to_burned_land] = 4
     grid[burnable_cells] = 5
+
+    if decision(1):
+        spotting(find_random_burning_cell(burning_array), [0, -1], 20)
+    # if decision(0.01):
+    #     lightning_strike()
 
     return grid
 
@@ -134,8 +248,11 @@ def setup(args):
 
     config.state_colors = np.array([chaparral_color, lake_color, dense_forest_color, scrubland_color, town_color, fire_color])/255
 
-    config.grid_dims = (200, 200)
-    config.num_generations = 500
+    grid_height = 200
+    grid_width = 200
+
+    config.grid_dims = (grid_width, grid_height)
+    config.num_generations = 100
 
     def draw_terrain():
         rows, cols = config.grid_dims
@@ -196,16 +313,36 @@ def setup(args):
     return config, initial_terrain
 
 
+    
+
 def main():
     # Open the config object
     config, initial_terrain = setup(sys.argv[1:])
+    count = np.array([0])
 
     # initialise the decay grid
     decaygrid = np.zeros(config.grid_dims)
     decaygrid.fill(2)
 
+    # Initialise topology Grid
+    max_height = 100
+ 
+    noise = np.random.normal(0, max_height, size=config.grid_dims)
+    topology_grid = abs(noise).astype(int)
+    smoothed_topology = gaussian_filter(topology_grid, sigma=10)
+
+    # Graph and Save the topology grid as png
+    plt.imshow(smoothed_topology, cmap='hot', interpolation='nearest')
+    fig1 = plt.gcf()
+    plt.colorbar()
+    plt.show()
+    plt.draw()
+    fig1.savefig('test.png', dpi=100)
+
+
+
     # Create grid object
-    grid = Grid2D(config, (transition_func, decaygrid, initial_terrain))
+    grid = Grid2D(config, (transition_func, decaygrid, initial_terrain, smoothed_topology, count))
 
     # Run the CA, save grid state every generation to timeline
     timeline = grid.run()
